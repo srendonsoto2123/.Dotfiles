dc2a53c3a9d8a18be7a0d1133c1bfc9b7cf21ae3
Sebastian Rendon <75395711+srendonsoto2123@users.noreply.github.com>
2025-02-09 01:34:29 -0500

    Configurando hyprland y kitty, pequeños cambios en starship

---
+++ .config/fish/conf.d/env.fish
@@ -0,0 +1,26 @@
+set -Ux SUDO_PROMPT "Password"
+set -Ux TERMINAL "kitty"
+set -Ux BROWSER "brave"
+set -Ux VISUAL "nvim"
+set -Ux EDITOR "nvim"
+
+
+set -Ux FZF_DEFAULT_OPTS "
+  --color fg:#cdd6f4
+  --color fg+:#cdd6f4
+  --color bg:#1e1e2e
+  --color bg+:#313244
+  --color hl:#f38ba8
+  --color hl+:#f38ba8
+  --color info:#cba6f7
+  --color prompt:#cba6f7
+  --color spinner:#f5e0dc
+  --color pointer:#f5e0dc
+  --color marker:#f5e0dc
+  --color border:#1e1e2e
+  --color header:#f38ba8
+  --prompt ' '
+  --pointer ' λ'
+  --layout=reverse
+  --border horizontal
+  --height 40"
