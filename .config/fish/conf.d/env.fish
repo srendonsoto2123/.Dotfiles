08d11da6012bd6310910d1d8d94d482de344a620
Sebastian Rendon <75395711+srendonsoto2123@users.noreply.github.com>
2025-02-11 00:35:59 -0500

    Añadiendo cambios en hypr, starship y fish

--- .config/fish/conf.d/env.fish
+++ .config/fish/conf.d/env.fish
@@ -1,11 +1,28 @@
-set -Ux SUDO_PROMPT "Password"
-set -Ux TERMINAL "kitty"
-set -Ux BROWSER "brave"
-set -Ux VISUAL "nvim"
-set -Ux EDITOR "nvim"
+set -gx GPG_TTY (tty)
 
+set -gx SUDO_PROMPT "󰌾 Password: "
+set -gx TERMINAL "kitty"
+set -gx BROWSER "brave"
+set -gx VISUAL "nvim"
+set -gx EDITOR "nvim"
 
-set -Ux FZF_DEFAULT_OPTS "
+set -gx XDG_CONFIG_HOME "$HOME/.config"
+set -gx XDG_CACHE_HOME "$HOME/.cache"
+set -gx XDG_DATA_HOME "$HOME/.local/share"
+set -gx XDG_STATE_HOME "$HOME/.local/state"
+set -gx XDG_CONFIG_DIRS "/etc/xdg"
+set -gx XDG_DATA_DIRS "/usr/local/share:/usr/share:/var/lib/flatpak/exports/share:$XDG_DATA_HOME/flatpak/exports/share"
+set -gx XDG_RUNTIME_DIR "/run/user/$(id -u)"
+set -gx XDG_DESKTOP_DIR "$HOME/Escritorio"
+set -gx XDG_DOWNLOAD_DIR "$HOME/Descargas"
+set -gx XDG_TEMPLATES_DIR "$HOME/Plantillas"
+set -gx XDG_PUBLICSHARE_DIR "$HOME/Public"
+set -gx XDG_DOCUMENTS_DIR "$HOME/Documents"
+set -gx XDG_MUSIC_DIR "$HOME/Music"
+set -gx XDG_PICTURES_DIR "$HOME/Pictures"
+set -gx XDG_VIDEOS_DIR "$HOME/Videos"
+
+set -gx FZF_DEFAULT_OPTS "
   --color fg:#cdd6f4
   --color fg+:#cdd6f4
   --color bg:#1e1e2e
